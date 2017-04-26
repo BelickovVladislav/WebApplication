ecd6a79b9044c280be938442be76354ef9f9bd0c
BelickovVlad <belickov.vlad1997@yandex.ru>
2017-04-26 15:36:47 +0300

    Add includeTemplate, abstarct Component

--- app/components/News.List/class.php
+++ app/components/News.List/class.php
@@ -1,11 +1,15 @@
 <?php
 
 
-class News extends Component
+class NewsList extends Component
 {
     public function __construct($name, $template, array $params = array())
     {
         parent::__construct($name, $template, $params);
     }
 
+    public function executeComponent()
+    {
+        // TODO: Implement executeComponent() method.
+    }
 }
