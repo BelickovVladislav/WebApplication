3d6fbea3b2b08384d25eb9a8fc75b8650e0ea228
BelickovVlad <belickov.vlad1997@yandex.ru>
2017-04-26 18:01:34 +0300

    Add Data

--- app/components/News.List/class.php
+++ app/components/News.List/class.php
@@ -10,6 +10,28 @@ class NewsList extends Component
 
     public function executeComponent()
     {
-        // TODO: Implement executeComponent() method.
+        $this->prepareParams();
+        $this->arrResult = $this->getResult($this->getPageNumber());
+        $this->includeTemplate();
+    }
+
+    protected function getResult($pageNumber)
+    {
+        $array = array();
+        if (file_exists($_SERVER['DOCUMENT_ROOT'] . '/app/data/' . $this->params['data'])) {
+            $xmlString = file_get_contents($_SERVER['DOCUMENT_ROOT'] . '/app/data/' . $this->params['data']);
+            $xml = new SimpleXMLElement($xmlString);
+            $i = 0;
+            foreach ($xml->item as $item) {
+                $array[$i]['title'] = $item->title->__toString();
+                $array[$i]['picture'] = $item->picture->__toString();;
+                $array[$i]['description'] = $item->description->__toString();;
+                $array[$i]['link'] = $item->link->__toString();;
+                $array[$i]['pubDate'] = $item->pubDate->__toString();;
+                $i++;
+            }
+        }
+        return array_splice($array, ($pageNumber - 1) * $this->params['count'], $this->params['count']);
+
     }
 }
