c3c9e5fdcbd37dd57b5525324c172949cfd2feab
BelickovVlad <belickov.vlad1997@yandex.ru>
2017-04-27 20:11:26 +0300

    Pagination

--- app/components/News.List/class.php
+++ app/components/News.List/class.php
@@ -15,13 +15,25 @@ class NewsList extends Component
         $this->arrResult = $this->getResult($this->getPageNumber(), $countElements);
         $this->includeTemplate();
         echo "<div class = 'pagination'>";
-        for ($i = 0; $i < (int)$countElements / $this->params['count']; $i++) {
-            if ($this->getPageNumber() != $i + 1) {
-                ?><a href="/news/page-<?= $i + 1 ?>/" class='pagination'><?= $i + 1 ?></a> <?php
-            } else {
-                ?><a class='active'><?= $i + 1 ?></a> <?php
-            }
+        $max = (int)$countElements / $this->params['count'];
+
+        echo "<a href=\"/news/page-1/\" ".($this->getPageNumber()==1 ?" class = 'active'":"").">1</a>";
+        if ($this->getPageNumber() > (int)$max / 2) {
+            echo "<a href=\"/news/page-1" . (int)(((int)$max / 2 - 1) / 2) . "/\" >...</a>";;
         }
+        for ($i = $this->getPageNumber() - 2; $i < $this->getPageNumber() + 1 && $i < $max - 1; $i++) {
+            if ($i > 0)
+                if ($this->getPageNumber() != $i + 1) {
+                    ?><a href="/news/page-<?= $i + 1 ?>/" ><?= $i + 1 ?></a> <?php
+                } else {
+                    ?><a class='active'><?= $i + 1 ?></a> <?php
+                }
+        }
+        if ($this->getPageNumber() < (int)$max / 2) {
+
+            echo "<a href=\"/news/page-" . (int)(((int)$max+$this->getPageNumber()) / 2) . "/\" >...</a>";
+        }
+        echo "<a href=\"/news/page-" . $max . "/\"".($this->getPageNumber()==$max?" class = 'active'":"")." >" . $max . "</a>";
         echo "</div>";
     }
 
